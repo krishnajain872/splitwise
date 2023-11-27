1719b60503f91c39d5c3324ac91f9ea75a1ba6aa
krishna jain <krishnajain@gkmit.co>
2023-11-27 15:07:02 +0530

    feat: add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+    },
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: ['.eslintrc.{js,cjs}'],
+        parserOptions: {
+          sourceType: 'script',
+        },
+      },
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+    },
+    rules: {},
+  };
