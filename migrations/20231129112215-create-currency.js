380bc0280c6e81686ec1088d6155d67e282661dd
krishna jain <krishnajain@gkmit.co>
2023-11-29 17:42:40 +0530

    fix: currency UUID and migration order

--- migrations/20231129112215-create-currency.js
+++ migrations/20231129112215-create-currency.js
@@ -5,9 +5,9 @@ module.exports = {
         await queryInterface.createTable('currencies', {
             id: {
                 allowNull: false,
-                autoIncrement: true,
                 primaryKey: true,
-                type: Sequelize.INTEGER,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.literal('uuid_generate_v4()'),
             },
             code: {
                 type: Sequelize.STRING,
