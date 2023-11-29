5cda37ccfad8a6f5aec4a6b47efb9d25cbcf857f
krishna jain <krishnajain@gkmit.co>
2023-11-29 14:05:20 +0530

    feat: expense assocaition with group

--- models/Expense.js
+++ models/Expense.js
@@ -7,8 +7,12 @@ module.exports = (sequelize, DataTypes) => {
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
-        static associate() {
-            // define association here
+        static associate(models) {
+            Expense.belongsTo(models.Group, {
+                as: 'group_details',
+                foreignKey: 'group_id',
+                targetKey: 'id',
+            })
         }
     }
     Expense.init(
