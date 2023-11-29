1688a4fb6ee34f718c658fd5d8165754829b6b51
krishna jain <krishnajain@gkmit.co>
2023-11-29 12:41:51 +0530

    fix: create group migration

--- migrations/20231128183359-create-group.js
+++ migrations/20231128183359-create-group.js
@@ -1,7 +1,7 @@
 'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface) {
+    async up(queryInterface, Sequelize) {
         await queryInterface.createTable('groups', {
             id: {
                 allowNull: false,
