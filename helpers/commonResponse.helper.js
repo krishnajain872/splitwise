256ae440a460246769869c966953b98f21977d04
krishna jain <krishnajain@gkmit.co>
2023-11-30 14:21:13 +0530

    fix: error and response helper

--- helpers/commonResponse.helper.js
+++ helpers/commonResponse.helper.js
@@ -1,18 +1,35 @@
-const errorHelper = (code, name, message, actual) => {
-    return {
-        code: code,
-        success: false,
-        name: name,
-        message: message,
-        actual: actual,
+const errorHelper = async (
+    req,
+    res,
+    message,
+    statusCode = 500,
+    error = null
+) => {
+    let errorMessage = 'Something went wrong. Please try again'
+    if (message) {
+        errorMessage = message
     }
+
+    if (error && error.message) {
+        errorMessage = error.message
+    }
+    req.error = error
+
+    const response = {
+        statusCode,
+        data: {},
+        message: errorMessage,
+    }
+    res.status(statusCode).json(response)
 }
-const responseHelper = (code, success, message, payload) => {
-    return {
+
+const responseHelper = (req, res, code, success, message, payload) => {
+    const response = {
         code: code,
         success: success,
-        data: { message: message, payload: payload },
+        data: { message: message || 'success', payload: payload },
     }
+    return res.status(code).json(response)
 }
 
 module.exports = {
