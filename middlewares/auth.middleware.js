3ba9371e5a1fe15606cb8afe7e25c630a8d410d5
krishna jain <krishnajain@gkmit.co>
2023-11-30 17:32:08 +0530

    feat: auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,79 @@
+const jwt = require('jsonwebtoken')
+const db = require('./../models')
+const User = db.User
+require('dotenv').config()
+const generic = require('./../helpers/commonResponse.helper')
+
+const checkAccessToken = async (req, res, next) => {
+    const accessToken = req.headers['authorization']?.split(' ')[1]
+
+    const { JWT_AUTH_TOKEN_SECRET: secret } = process.env
+
+    if (!accessToken) {
+        throw new Error('UnAuthorized Access')
+    }
+    try {
+        const decodedJwt = await jwt.verify(accessToken, secret)
+
+        const user = await User.findByPk({
+            where: {
+                id: decodedJwt.id,
+            },
+        })
+
+        if (!user) {
+            const error = new Error('User not found')
+            error.statusCode = 404
+            throw error
+        }
+    } catch (error) {
+        return generic.errorHelper(
+            req,
+            res,
+            error.message,
+            error.statusCode,
+            error
+        )
+    }
+
+    next()
+}
+const checkRefreshToken = async (req, res, next) => {
+    const refresh = req.body.refresh_token
+
+    const { JWT_REFRESH_TOKEN_SECRET: secret } = process.env
+
+    if (!accessToken) {
+        throw new Error('UnAuthorized Access')
+    }
+    try {
+        const decodedJwt = await jwt.verify(refresh, secret)
+
+        const user = await User.findByPk({
+            where: {
+                id: decodedJwt.id,
+            },
+        })
+
+        if (!user) {
+            const error = new Error('User not found')
+            error.statusCode = 404
+            throw error
+        }
+    } catch (error) {
+        return generic.errorHelper(
+            req,
+            res,
+            error.message,
+            error.statusCode,
+            error
+        )
+    }
+
+    next()
+}
+
+module.exports = {
+    checkAccessToken,
+    checkRefreshToken,
+}
