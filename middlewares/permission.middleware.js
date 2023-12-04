56336cce6eb7ab1398385f550b1d1bc488b2c3d7
krishna jain <krishnajain@gkmit.co>
2023-12-04 23:41:08 +0530

    fix: check by debt

--- middlewares/permission.middleware.js
+++ middlewares/permission.middleware.js
@@ -1,6 +1,5 @@
 const { User } = require('../models')
 const { Group } = require('../models')
-const { Transaction } = require('../models')
 
 const checkPermission = () => {
     return async (req, res, next) => {
@@ -59,54 +58,7 @@ const checkPermissionByRegistrationStatus = () => {
     }
 }
 
-const checkPermissionByTransactionDebt = () => {
-    return async (req, res, next) => {
-        try {
-            const group_id = req.body.group_id
-            const existingGroup = await User.findByPk(group_id)
-            if (!existingGroup) {
-                const error = new Error('group not found')
-                error.statusCode = 404
-                throw error
-            }
-            // eager loading
-            let totalPendingAmount = 0
-
-            const groupExpence = await Group.findAll({
-                include: {
-                    model: Expense,
-                    as: 'group_expenses',
-                    include: {
-                        model: Transaction,
-                        as: 'transaction',
-                        where: { settle_up_at: null },
-                    },
-                },
-                where: { id: group_id },
-            })
-            groupExpence.forEach((group) => {
-                group.group_expenses.forEach((expense) => {
-                    expense.transaction.forEach((transaction) => {
-                        totalPendingAmount =
-                            Number(transaction.dataValues.amount) +
-                            Number(totalPendingAmount)
-                    })
-                })
-            })
-            if (totalPendingAmount > 0) {
-                const error = new Error('pending debts in group')
-                error.statusCode = 409
-                throw error
-            } else {
-                next()
-            }
-        } catch (error) {
-            commonErrorHandler(req, res, error.message, res.statusCode, error)
-        }
-    }
-}
 module.exports = {
     checkPermission,
     checkPermissionByRegistrationStatus,
-    checkPermissionByTransactionDebt,
 }
