58e3676dfb65be9ecefc90987f06f7d4605edd0f
krishna jain <krishnajain@gkmit.co>
2023-12-25 15:46:08 +0530

    feat: role check and permission middleware

---
+++ middlewares/permission.middleware.js
@@ -0,0 +1,181 @@
+const { User } = require('../models')
+const { Group } = require('../models')
+const { UserGroup } = require('../models')
+const { Payee } = require('../models')
+const { Expense } = require('../models')
+const { Transaction } = require('../models')
+
+const { errorHelper } = require('../helpers/commonResponse.helper')
+
+// all the permission for admin are going to be check by this
+const checkPermission = async (req, res, next) => {
+    try {
+        const { id: group_id } = req.params.value
+        const { id: admin_id } = req.user
+        const existingGroup = await Group.findByPk(group_id)
+        if (!existingGroup) {
+            const error = new Error('group not found')
+            error.statusCode = 404
+            throw error
+        }
+        if (existingGroup.admin_id === admin_id) {
+            next()
+        } else {
+            const error = new Error('unAuthorized Access')
+            error.statusCode = 403
+            throw error
+        }
+    } catch (error) {
+        errorHelper(req, res, error.message, error.statusCode, error)
+    }
+}
+
+// all the permissions check by this for only verified user
+const checkPermissionByRegistrationStatus = async (req, res, next) => {
+    try {
+        const { status } = req.user
+        if (status === 'dummy' || status === 'unVerified') {
+            const error = new Error('user is not verified')
+            error.statusCode = 403
+            throw error
+        } else {
+            next()
+        }
+    } catch (error) {
+        errorHelper(req, res, error.message, error.statusCode, error)
+    }
+}
+
+// all the permissions check by this for all the valid  group members
+const checkPermissionByValidGroupMember = async (req, res, next) => {
+    try {
+        const { id: user_id } = req.user
+        const { id: group_id } = req.params.value
+        const existGroup = await Group.findByPk(group_id)
+        console.log('THIS IS GROUP ID WHEN ADD EXPENSE ==> 1234@1234', {
+            existGroup,
+            group_id,
+        })
+        if (!existGroup || existGroup === null) {
+            const error = new Error('group not found')
+            error.statusCode = 404
+            throw error
+        }
+        const existingUser = await UserGroup.findOne({
+            include: [
+                {
+                    model: User,
+                    as: 'user_details',
+                },
+            ],
+            where: {
+                user_id,
+                group_id,
+            },
+        })
+
+        if (!existingUser) {
+            const error = new Error(
+                'unAuthorized access user is not part of the group'
+            )
+            error.statusCode = 403
+            throw error
+        }
+        if (
+            existingUser.user_details.status === 'dummy' ||
+            existingUser.user_details.status === 'unVerified'
+        ) {
+            const error = new Error('user is not verified')
+            error.statusCode = 401
+            throw error
+        } else {
+            next()
+        }
+    } catch (error) {
+        errorHelper(req, res, error.message, error.statusCode, error)
+    }
+}
+// all the permissions check by this for all the valid  group members
+const checkPermissionByValidExpenseMember = async (req, res, next) => {
+    try {
+        const { id: user_id, status } = req.user
+        const { expense_id: expense } = req.params.value
+        const existingExpense = await Expense.findByPk(expense)
+        if (!existingExpense) {
+            const error = new Error('Expense not found')
+            error.statusCode = 404
+            throw error
+        }
+        const existingPayee = await Payee.findOne({
+            where: {
+                user_id,
+                expense_id: expense,
+            },
+        })
+        if (!existingPayee) {
+            const error = new Error(
+                'unauthorized access user is not part of the expense'
+            )
+            error.statusCode = 403
+            throw error
+        }
+
+        if (status === 'dummy' || status === 'unVerified') {
+            const error = new Error('user is not verified')
+            error.statusCode = 401
+            throw error
+        } else {
+            next()
+        }
+    } catch (error) {
+        errorHelper(req, res, error.message, error.statusCode, error)
+    }
+}
+const checkPermissionByTransactionDebt = async (req, res, next) => {
+    try {
+        const { id: group_id } = req.params.value
+        const existingGroup = await Group.findByPk(group_id)
+
+        if (!existingGroup) {
+            const error = new Error('group not found')
+            error.statusCode = 404
+            throw error
+        }
+
+        let totalPendingAmount = 0
+
+        const groupExpenses = await Expense.findAll({
+            where: { group_id },
+            include: {
+                model: Transaction,
+                as: 'transaction',
+                where: { settle_up_at: null },
+                attributes: ['amount'],
+            },
+        })
+
+        groupExpenses.forEach((expense) => {
+            expense.transaction.forEach((transaction) => {
+                totalPendingAmount += Number(transaction.amount)
+            })
+        })
+
+        if (totalPendingAmount > 0) {
+            const error = new Error('pending debts in group')
+            error.statusCode = 409
+            throw error
+        } else {
+            next()
+        }
+    } catch (error) {
+        errorHelper(req, res, error.message, error.statusCode, error)
+    }
+}
+
+module.exports = {
+    checkPermission,
+    checkPermissionByRegistrationStatus,
+    checkPermissionByTransactionDebt,
+    checkPermissionByValidGroupMember,
+    checkPermissionByValidExpenseMember,
+}
